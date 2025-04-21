fe29c35c21b0d037be95a30e4fc27fd3c63a0f4a
claudia rivas <usr3860@salesianos-lacuesta.net>
2025-04-21 17:10:12 +0100

    feat: Creation of AdviceEntity entity

---
+++ src/main/java/com/healthybites/entity/AdviceEntity.java
@@ -0,0 +1,48 @@
+package com.healthybites.entity;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "advice")
+public class AdviceEntity {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 120)
+	private String title;
+	
+	@Column(nullable = false, length = 200)
+	private String description;
+	
+	@Column(nullable = false)
+	private LocalDateTime creationDate;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	@JsonBackReference
+    private UserEntity user;
+	
+}
