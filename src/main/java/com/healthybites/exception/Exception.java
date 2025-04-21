0af25fadc70ceecc711658da7b607e7f9f0e773f
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2025-04-21 16:29:40 +0100

    feat(): Add global exception handler using ApiError.

---
+++ src/main/java/com/healthybites/exception/Exception.java
@@ -0,0 +1,13 @@
+package com.healthybites.exception;
+
+public class Exception extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public Exception(String message) {
+		super(message);		
+	}
+	
+	public Exception(String message, Throwable cause) {
+		super(message, cause);		
+	}
+}
