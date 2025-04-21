77cc1c97908d58a48b88e365964bf509b1ec6c68
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2025-04-21 13:00:33 +0100

    feat(InfoUser): Add attributes of the InfoUser entity

---
+++ src/main/java/com/healthybites/entity/InfoUserEntity.java
@@ -0,0 +1,41 @@
+package com.healthybites.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "info_user")
+public class InfoUserEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "height", nullable = false)
+	private Double height;
+	
+	@Column(name = "weight", nullable = false)
+	private Double weight;
+	
+	@Column(name = "activity_level", length = 30)
+	private String activityLevel;
+	
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private UserEntity user;
+	
+}
