65ed28226919a0d9ec6ae3f7f9439be716437c7d
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2025-04-21 21:04:03 +0100

    feat(): Add user service and CRUD.

---
+++ src/main/java/com/healthybites/service/user/UserService.java
@@ -0,0 +1,16 @@
+package com.healthybites.service.user;
+
+import java.util.List;
+
+import com.healthybites.dtos.user.UserEntityRequestDto;
+import com.healthybites.dtos.user.UserEntityResponseDto;
+
+public interface UserService {
+	
+	UserEntityResponseDto createUser(UserEntityRequestDto userRequestDto);
+	List<UserEntityResponseDto> getAllUsers();
+	UserEntityResponseDto getUserById(Long id);
+	UserEntityResponseDto updateUser(Long id, UserEntityRequestDto userRequestDto);
+	void deleteUser(Long id);
+	
+}
