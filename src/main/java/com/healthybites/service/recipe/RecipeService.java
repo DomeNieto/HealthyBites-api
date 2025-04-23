37b11e14f7727903e5e733f3ce7517b2f7cd1c1e
claudia rivas <usr3860@salesianos-lacuesta.net>
2025-04-23 10:01:19 +0100

    feat: add IngredientService, RecipeService

---
+++ src/main/java/com/healthybites/service/recipe/RecipeService.java
@@ -0,0 +1,19 @@
+package com.healthybites.service.recipe;
+
+import java.util.List;
+
+import com.healthybites.dtos.ingredient.IngredientResponseDto;
+import com.healthybites.dtos.recipe.RecipeRequestDto;
+import com.healthybites.dtos.recipe.RecipeResponseDto;
+
+public interface RecipeService {
+
+	public List<RecipeResponseDto> getAllRecipes();
+	public RecipeResponseDto createRecipe(RecipeRequestDto recipeRequestDto);
+	public RecipeResponseDto updateRecipe(Long recipeId, RecipeRequestDto recipeRequest);
+	public void deleteRecipe(Long recipeId);
+	public boolean deleteIngredientToRecipe(Long recipeId, Long ingredientId);
+	public List<IngredientResponseDto> getIngredientsForRecipe(Long recipeId);
+	public List<RecipeResponseDto> getAllRecipesByUser(Long userId);
+	public RecipeResponseDto getRecipeById(Long recipeId);
+}
